d3b26624857e8d454b8fd921a7a90267d7b6f33b
AleksanderKuzmin <alex.kuzminn@gmail.com>
2020-12-21 20:01:53 -0500

    Model Entry created

--- learning_log/learning_logs/admin.py
+++ learning_log/learning_logs/admin.py
@@ -3,3 +3,4 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 admin.site.register(Topic)
+admin.site.register(Entry)
--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -9,3 +9,18 @@ class Topic(models.Model):
     def __str__(self):
         """Return a string representation of te model."""
         return self.text
+
+
+class Entry(models.Model):
+    """Specific entry leard about a Topic."""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Retutn a string representation of the model Entry."""
+        return f"{self.text[:50]}..."
+
