13fa3a08a29829e84a8bde6ef4b47dec3e284612
AleksanderKuzmin <alex.kuzminn@gmail.com>
2020-12-22 13:36:57 -0500

    Topics page added

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -7,4 +7,6 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Page for all topics.
+    path('topics/', views.topics, name='topics'),
 ]
